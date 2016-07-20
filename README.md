7bdbd7bcffab5d427ad14ee2336fc95403c14e24
Rhadow <howard3433@gmail.com>
2016-07-20 20:45:51 +0800

    Add facebook webhook

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MrRobot
+[![Build Status](https://travis-ci.org/Rhadow/MrRobot.svg?branch=master)](https://travis-ci.org/Rhadow/MrRobot)
+
+> A simple Facebook messenger bot
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 'use strict';
 
-let express = require('express');
-let app = express();
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const VALIDATION_TOKEN = process.env.VALIDATION_TOKEN;
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send('Hey there!!!!');
+    res.send('Hello world, I am Mr. Robot, your best helper!');
+});
+
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        console.log('Validating webhook');
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error('Failed validation. Make sure the validation tokens match.');
+        res.sendStatus(403);
+    }
 });
 
 app.listen(app.get('port'), () => {
