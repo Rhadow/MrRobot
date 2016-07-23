938958f99b87ac842f741ac2f3337305a2efe2d2
Rhadow <howard3433@gmail.com>
2016-07-23 13:27:17 +0800

    Add content type header for json object

--- functions/sendTextMessage.js
+++ functions/sendTextMessage.js
@@ -12,19 +12,24 @@ const sendTextMessage = (recipientId, messageText) => {
 
     fetch(`${CONSTANTS.FB_MESSAGE_API_URL}?access_token=${process.env.FB_PAGE_ACCESS_TOKEN}`, {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(messageData)
     }).then((res) => {
         if (res.status >= 400) {
-            console.error('Unable to send message.');
-            console.error(res);
+            let error = new Error(res.statusText);
+            error.response = res;
+            throw error;
         } else {
             return res.json();
         }
     }).then((data) => {
-        console.log(data);
         const recipientId = data['recipient_id'];
         const messageId = data['message_id'];
         console.log(`Successfully sent text message with id ${messageId} to recipient ${recipientId}`);
+    }).catch((error) => {
+        console.error('request failed', error);
     });
 };
 
